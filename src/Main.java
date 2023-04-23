a211f65c230dc69a17119d898e0d7b1aebf46420
Chris Castle <chrisjcastle93@gmail.com>
2023-04-23 20:01:50 +0200

    x

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create some arrays
+        String[] wordListOne = {"agnostic", "opinionated", "voice activated", "haptically driven", "extensible", "reactive", "agent based", "functional", "AI enabled", "strongly typed"};
+        String[] wordListTwo = {"loosely coupled", "six sigma", "asynchronous", "event driven", "pub-sub", "IoT", "cloud native", "service oriented", "containerized", "serverless", "microservices", "distributed ledger"};
+        String[] wordListThree = {"framework", "library", "DSL", "REST API", "repository", "pipeline", "service                mesh", "architecture", "perspective", "design", "orientation"};
+
+        // get length of each array
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        // Get a random word from each of them
+        java.util.Random randomGenerator = new java.util.Random();
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+
+        // now build a phrase
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        // print out the phrase
+        System.out.println("What we need is a " + phrase);
+        // var i = new Console();
+        // i.log(phrase);
     }
 }
+
+//class Console {
+//    void log(String phrase) {
+//        // print out the phrase
+//        System.out.println("What we need is a " + phrase);
+//    }
+//}
